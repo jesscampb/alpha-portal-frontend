c9f01e667d1ce6ba9159967b3aabf1813f9aa69f
Jessica Campbell <jesscampb91@gmail.com>
2025-04-18 23:50:32 +0200

    Added VITE at the start of key names. Tried fetching data from different endpoints in AuthContext. Works.

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -3,21 +3,32 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
+const apiKey = import.meta.env.VITE_DEFAULT_API_KEY
+const adminApiKey = import.meta.env.VITE_ADMIN_API_KEY
+
 export const AuthProvider = ({ children }) => {
-    const apiEndpoint = "http://localhost:5246/api/projects"
-    // role är null, istället för "admin", för jag har ingen rollhantering
-    const defaultValues = { accessToken: null, role: null, isAuthenticated: true, loading: false }
+    const apiEndpoint = "https://localhost:7141/api/users"
+    const defaultValues = { accessToken: null, role: "admin", isAuthenticated: true, loading: false }
     const [auth, setAuth] = useState(defaultValues)
 
     const fetchAuthData = async () => {
       setAuth({ ...defaultValues, loading: false })
     
         try {
-          const res = await fetch(apiEndpoint)
+          const res = await fetch(apiEndpoint, {
+            headers: {
+              "X-API-KEY": apiKey,
+              "X-ADM-API-KEY": adminApiKey
+            },
+            method: "GET",
+
+          })
 
           if (res.ok) {
             const data = await res.json();
+            // role är "admin", istället för data.role, för jag har ingen rollhantering
             setAuth({ accessToken: data.accessToken, role: data.role, isAuthenticated: true, loading: false });
+            console.log(data)
           }  
 
         } catch (error) {
