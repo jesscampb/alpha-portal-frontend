6a46ecba5f48f3e4bfa489be7c2dbcc8a8d55ee5
Jessica Campbell <jesscampb91@gmail.com>
2025-04-20 13:59:57 +0200

    Added getUsers, getClients, getProjectStatuses

--- src/contexts/ProjectContext.jsx
+++ src/contexts/ProjectContext.jsx
@@ -7,21 +7,27 @@ export const useProject = () => useContext(ProjectContext)
 export const ProjectProvider = ({children}) => {
   const {apiKey} = useAuth()
 
-  const apiUri = `${import.meta.env.VITE_API_URI}/projects`
+  const apiUri = import.meta.env.VITE_API_URI
   const [projects, setProjects] = useState([])
   const [users, setUsers] = useState([])
   const [clients, setClients] = useState([])
-  const [projectStatus, setProjectStatus] = useState([])
+  const [projectStatuses, setProjectStatuses] = useState([])
   const [loading, setLoading] = useState(false)
 
   projectData = {
     projects,
-    getProjects
+    getProjects,
+    users,
+    getUsers,
+    clients,
+    getClients,
+    projectStatuses,
+    getProjectStatuses
   }
 
   const getProjects = async () => {
     try {
-      const res = await fetch(apiuri, {
+      const res = await fetch(`${apiuri}/projects`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -37,6 +43,64 @@ export const ProjectProvider = ({children}) => {
       console.log('Error fetching projects:', error)
     }
   }
+
+  const getClients = async () => {
+    try {
+      const res = await fetch(`${apiuri}/clients`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-Key': apiKey
+        }
+      })
+
+      if (res.ok) {
+        const data = await res.json()
+        setClients(data)
+      }
+    } catch (error) {
+      console.log('Error fetching clients:', error)
+    }
+  }
+
+  const getUsers = async () => {
+    try {
+      const res = await fetch(`${apiuri}/users`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-Key': apiKey
+        }
+      })
+
+      if (res.ok) {
+        const data = await res.json()
+        setUsers(data)
+      }
+    } catch (error) {
+      console.log('Error fetching users:', error)
+    }
+  }
+
+  const getProjectStatuses = async () => {
+    try {
+      const res = await fetch(`${apiuri}/projectStatuses`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-Key': apiKey
+        }
+      })
+
+      if (res.ok) {
+        const data = await res.json()
+        setProjectStatuses(data)
+      }
+    } catch (error) {
+      console.log('Error fetching project statuses:', error)
+    }
+  }
+
 
   return (
     <ProjectContext.Provider value={projectData}>
