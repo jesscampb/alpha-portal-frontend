5bffb0d4250e0613f42a5af998cd41c991fcbf9a
Jessica Campbell <jesscampb91@gmail.com>
2025-04-20 10:48:49 +0200

    Minimized AuthContext to only handle keys and loading state.

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,48 +1,21 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
-const apiUrl = import.meta.env.VITE_API_URL
 const apiKey = import.meta.env.VITE_DEFAULT_API_KEY
 const adminApiKey = import.meta.env.VITE_ADMIN_API_KEY
 
 export const AuthProvider = ({ children }) => {
-    const apiEndpoint = `${apiUrl}/projects`
-    const defaultValues = { accessToken: null, role: "admin", isAuthenticated: true, loading: false }
-    const [auth, setAuth] = useState(defaultValues)
+  const [loading, setLoading] = useState(false)
+  const authKeys = {
+    apiKey: apiKey,
+    adminApiKey: adminApiKey
+  }
 
-    const fetchAuthData = async () => {
-      setAuth({ ...defaultValues, loading: false })
-    
-        try {
-          const res = await fetch(apiEndpoint, {
-            headers: {
-              "X-API-KEY": apiKey,
-              "X-ADM-API-KEY": adminApiKey
-            },
-            method: "GET",
-
-          })
-
-          if (res.ok) {
-            const data = await res.json();
-            setAuth({ accessToken: data.accessToken, role: data.role, isAuthenticated: true, loading: false });
-            console.log(data)
-          }  
-
-        } catch (error) {
-          setAuth(defaultValues)
-        }
-    }
-  
-    useEffect(() => {
-        fetchAuthData()
-    }, [])
-
-    return (
-        <AuthContext.Provider value={{auth}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={{authKeys, loading, setLoading}}>
+        {children}
+    </AuthContext.Provider>
+  )
 }
