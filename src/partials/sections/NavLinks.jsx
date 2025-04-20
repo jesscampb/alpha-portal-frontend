7705e33ad2c48b7f73fbc89a4ed895bb797df673
Jessica Campbell <jesscampb91@gmail.com>
2025-04-20 11:15:49 +0200

    role instead of auth.role in NavLinks. Site works again!

--- src/partials/sections/NavLinks.jsx
+++ src/partials/sections/NavLinks.jsx
@@ -3,13 +3,13 @@ import NavLinkItem from '../components/NavLinkItem'
 import { useAuth } from '../../contexts/AuthContext'
 
 const NavLinks = () => {
-  const {auth} = useAuth()
+  const {role} = useAuth()
 
   return (
     <nav className="nav-links">
         <NavLinkItem to="/admin/projects" text="Projects" iconClass="fa-duotone fa-solid fa-briefcase"  />
         
-        {auth.role === "admin" && (
+        {role === "admin" && (
         <>
           <NavLinkItem to="/admin/members" text="Members" iconClass="fa-duotone fa-solid fa-user-group" />
           <NavLinkItem to="/admin/clients" text="Clients" iconClass="fa-duotone fa-solid fa-handshake" />
